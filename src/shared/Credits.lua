ac49c77b4bdc139ffc86ba4f7b8d59199b78f922
Senko3141 <68160877+SenkoDeveloper@users.noreply.github.com>
2021-03-29 19:35:29 -0400

    Update 2.2 / Added credits on md

--- src/shared/Credits.lua
+++ src/shared/Credits.lua
@@ -1,3 +1,7 @@
+-- Credits md
+-- Senko
+-- 3/29/2021
+
 return function ()
 	warn("[SenkSabers Tech] \n[INFO]Lightsaber tech scripted by SenkoArdeth.\n [CREDITS] SenkoArdeth - Developer\nThank you for using SenkSabers! If there are any bugs please DM Senko#6389.")
 end
